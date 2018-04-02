a949a7f9fd3b8df824c6410ec6e524a042e1e3c2
jttalkin <jttalkin@go.olemiss.edu>
2018-04-02 02:43:15 -0500

    fixed and completed SQL file

--- seniorProjectInventorySystem.sql
+++ seniorProjectInventorySystem.sql
@@ -1,3 +1,13 @@
+ALTER TABLE Login DROP FOREIGN KEY Login_User;
+ALTER TABLE Inventory DROP FOREIGN KEY Inventory_Location;
+ALTER TABLE Inventory DROP FOREIGN KEY Inventory_Category;
+ALTER TABLE Inventory DROP FOREIGN KEY Inventory_User;
+ALTER TABLE Discarded DROP FOREIGN KEY Discarded_Location;
+ALTER TABLE Discarded DROP FOREIGN KEY Discarded_Category;
+ALTER TABLE Discarded DROP FOREIGN KEY Discarded_User;
+
+DROP TABLE IF EXISTS User, Login, Inventory, Discarded, Location, Category; 
+
 --
 -- Table structure for table User
 --
@@ -13,37 +23,9 @@ CREATE TABLE User (
 -- Table structure for table Login
 --
 CREATE TABLE Login (
-  Username varchar(15) NOT NULL AUTO_INCREMENT,
+  Username varchar(15) NOT NULL,
   Password varchar(60) DEFAULT NULL,
   PRIMARY KEY (Username)
-)ENGINE=INNODB;
-
---
--- Table structure for table Inventory
---
-CREATE TABLE Inventory (
-  InventoryNum int(11) NOT NULL AUTO_INCREMENT,
-  Name varchar(30) DEFAULT NULL,
-  LocationID int(11) DEFAULT NULL,
-  CategoryID int(11) DEFAULT NULL,
-  Quanity int(11) DEFAULT NULL,
-  LastQuanityUpdate smalldatetime DEFAULT NULL,
-  Username varchar(15) DEFAULT NULL,
-  PRIMARY KEY (InventoryNum)
-)ENGINE=INNODB;
-
---
--- Table structure for table Discarded
---
-CREATE TABLE Discarded (
-  DiscardedInventoryNum int(11) NOT NULL AUTO_INCREMENT,
-  Name varchar(30) DEFAULT NULL,
-  LocationID int(11) DEFAULT NULL,
-  CategoryID int(11) DEFAULT NULL,
-  QuanityDiscarded int(11) DEFAULT NULL,
-  LastDiscardUpdate smalldatetime DEFAULT NULL,
-  Username varchar(15) DEFAULT NULL,
-  PRIMARY KEY (DiscardedInventoryNum)
 )ENGINE=INNODB;
 
 --
@@ -64,3 +46,60 @@ CREATE TABLE Category (
   PRIMARY KEY (CategoryID)
 )ENGINE=INNODB;
 
+--
+-- Table structure for table Inventory
+--
+CREATE TABLE Inventory (
+  InventoryNum int(11) NOT NULL AUTO_INCREMENT,
+  Name varchar(30) DEFAULT NULL,
+  LocationID int(11) DEFAULT NULL,
+  CategoryID int(11) DEFAULT NULL,
+  Quanity int(11) DEFAULT NULL,
+  LastQuanityUpdate datetime DEFAULT NULL,
+  Username varchar(15) NOT NULL,
+  PRIMARY KEY (InventoryNum)
+)ENGINE=INNODB;
+
+--
+-- Table structure for table Discarded
+--
+CREATE TABLE Discarded (
+  DiscardedInventoryNum int(11) NOT NULL AUTO_INCREMENT,
+  Name varchar(30) DEFAULT NULL,
+  LocationID int(11) DEFAULT NULL,
+  CategoryID int(11) DEFAULT NULL,
+  QuanityDiscarded int(11) DEFAULT NULL,
+  LastDiscardUpdate datetime DEFAULT NULL,
+  Username varchar(15) NOT NULL,
+  PRIMARY KEY (DiscardedInventoryNum)
+)ENGINE=INNODB;
+
+
+-- Foreign Keys
+-- Reference: Username (table: Login)
+ALTER TABLE Login ADD CONSTRAINT Login_User FOREIGN KEY Login_User (Username)
+    REFERENCES User (Username);
+
+-- Reference: Location (table: Inventory)
+ALTER TABLE Inventory ADD CONSTRAINT Inventory_Location FOREIGN KEY Inventory_Location (LocationID)
+    REFERENCES Location (LocationID);
+
+-- Reference: Caetegory (table: Inventory)
+ALTER TABLE Inventory ADD CONSTRAINT Inventory_Category FOREIGN KEY Inventory_Category (CategoryID)
+    REFERENCES Category (CategoryID);
+
+-- Reference: Username (table: Inventory)
+ALTER TABLE Inventory ADD CONSTRAINT Inventory_User FOREIGN KEY Inventory_User (Username)
+    REFERENCES User (Username);
+
+-- Reference: Location (table: Discarded)
+ALTER TABLE Discarded ADD CONSTRAINT Discarded_Location FOREIGN KEY Discarded_Location (LocationID)
+    REFERENCES Location (LocationID);
+
+-- Reference: Caetegory (table: Discarded)
+ALTER TABLE Discarded ADD CONSTRAINT Discarded_Category FOREIGN KEY Discarded_Category (CategoryID)
+    REFERENCES Category (CategoryID);
+
+-- Reference: Username (table: Discarded)
+ALTER TABLE Discarded ADD CONSTRAINT Discarded_User FOREIGN KEY Discarded_User (Username)
+    REFERENCES User (Username);
