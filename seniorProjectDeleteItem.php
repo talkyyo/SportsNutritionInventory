06058a12c6faa52726951c17f8fb2efa59bb673b
jttalkin <jttalkin@go.olemiss.edu>
2018-04-17 03:26:57 -0500

    added page to delete items

---
+++ seniorProjectDeleteItem.php
@@ -0,0 +1,38 @@
+<?php
+	//require session and required functions
+	require_once('seniorProjectSession_Functions.php');
+	require_once('seniorProjectRequiredFunctions.php');
+
+	//if error or alert is found, print it
+	if (($message = message()) !== null) {
+		echo $message;
+	}
+
+	//calls method to establish connection to server
+	$mysqli = databaseConnection();
+
+	if(isset($_GET["itemID"]) && $_GET["itemID"] !== "") {
+		$deleteItem = $_GET["itemID"];
+
+		$query = "DELETE FROM Inventory ";
+		$query .= "WHERE InventoryNum = ".$deleteItem;
+
+		$result = $mysqli->query($query);
+
+		if ($result) {
+			$_SESSION["alert"] = "Item successfully deleted!";
+			header("Location: seniorProjectLanding.php");
+		}
+		else {
+			$_SESSION["error"] = "Item could not be deleted!";
+			header("Location: seniorProjectLanding.php");
+			exit;
+		}
+	}
+	else {
+		$_SESSION["error"] = "Item could not be found!";
+		header("Location: seniorProjectLanding.php");
+		exit;
+	}
+
+?>
