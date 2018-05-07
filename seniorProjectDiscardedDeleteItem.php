663c5d482e50611c996ac580386386535e48176c
jttalkin <jttalkin@go.olemiss.edu>
2018-05-06 22:21:55 -0500

    fixed problem of deleting regular invntory instead of discarded

---
+++ seniorProjectDiscardedDeleteItem.php
@@ -0,0 +1,41 @@
+<?php
+	//require session and required functions
+	require_once('seniorProjectSession_Functions.php');
+	require_once('seniorProjectRequiredFunctions.php');
+
+	//if error or alert is found, print it
+	if (($message = message()) !== null) {
+		echo $message;
+	}
+
+	//calls method to establish connection to server
+	$mysqli = databaseConnection();
+
+	//verify that session ID exists
+  	verifyLogin();
+
+	if(isset($_GET["itemID"]) && $_GET["itemID"] !== "") {
+		$deleteItem = $_GET["itemID"];
+
+		$query = "DELETE FROM Discarded ";
+		$query .= "WHERE DiscardedInventoryNum = ".$deleteItem;
+
+		$result = $mysqli->query($query);
+
+		if ($result) {
+			$_SESSION["alert"] = "Item successfully deleted!";
+			header("Location: seniorProjectDiscarded.php");
+		}
+		else {
+			$_SESSION["error"] = "Item could not be deleted!";
+			header("Location: seniorProjectDiscarded.php");
+			exit;
+		}
+	}
+	else {
+		$_SESSION["error"] = "Item could not be found!";
+		header("Location: seniorProjectLanding.php");
+		exit;
+	}
+
+?>
